803f27507c4028e1339c17ea02e0213e1b3556e2
Qi Chen <qichen@qis-macbook-pro.home>
2025-03-16 19:22:01 -0400

    update

--- diffusion/run_cifar10.sh
+++ diffusion/run_cifar10.sh
@@ -3,15 +3,11 @@
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 10 on Béluga, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=0-02:00     # DD-HH:MM:SS
-#SBATCH --account=def-florian7
-module load python/3.10 cuda cudnn
 
-SOURCEDIR=~/scratch/diffusion_bound/
 
-# Prepare virtualenv
-virtualenv --no-download ~/ENV
-source ~/ENV/bin/activate
-export PYTHONPATH="/home/cq92/scratch/DCML/:$PYTHONPATH"
+'''
+add your cluster settings here
+'''
 
 # Start training
 
@@ -24,8 +20,5 @@ data="cifar10"
 
 sh run.sh $seed $T $R $data $n_train $n_iter
 
-#python src/img_bound_estimation.py --dataset=mnist --print_every 2000 --num_steps 1000 --n_train 100 --n_test 100 --mc_samples 100 \
-#	--sm_training_iter 10000 --sample_every 2000 --checkpoint_every 5000 --real=True --debias=False --lr=0.0001 \
-#	--seed $seed --T0 $T -R $R --train_batch_size 128 --test_batch_size 128
 
 
